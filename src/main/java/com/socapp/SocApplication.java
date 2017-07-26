03d06188502a1797ff02f863dc4f45bc13c3a93c
Pavel Aliakseyenka <pavel.aliakseyenka@ericsson.com>
2017-07-26 14:50:52 +0300

    Basic spring boot web application

---
+++ src/main/java/com/socapp/SocApplication.java
@@ -0,0 +1,11 @@
+package com.socapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SocApplication.class, args);
+    }
+}
