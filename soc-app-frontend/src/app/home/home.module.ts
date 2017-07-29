6ccadb4b7af709c7e8430f34431afe6db163954d
mikuhann <ng.kano@gmail.com>
2017-07-29 14:28:29 +0200

    user-list component
    User-list

--- soc-app-frontend/src/app/home/home.module.ts
+++ soc-app-frontend/src/app/home/home.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   imports: [
     SharedModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, UserListComponent]
 })
 export class HomeModule { }
