f75664e24778bea745833debb4b5bf2517a09df5
mikuhann <ng.kano@gmail.com>
2017-08-01 14:59:50 +0200

    Routing

--- soc-app-frontend/src/app/home/home-routing.module.ts
+++ soc-app-frontend/src/app/home/home-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 export const HOME_ROUTES: Routes = [
-	{
+ 	{
 		path: '',
 		component: HomeComponent,
 		children: [
 			// { path: '', component: Component },
-
+      { path: 'users', component: UserListComponent },
 			{ path: '**', redirectTo: '' }
 		]
 	},
--- soc-app-frontend/src/app/home/user-list/user-list.component.ts
+++ soc-app-frontend/src/app/home/user-list/user-list.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  constructor() {  }
 
   ngOnInit() {
-  }
+  }};
 
-}
+
