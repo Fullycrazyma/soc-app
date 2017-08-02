0fdfd9371fd7e328ebbace0063464c78acda07ad
mikuhann <ng.kano@gmail.com>
2017-08-03 01:33:57 +0200

    add user data

--- soc-app-frontend/src/app/home/user-list/user-list.component.ts
+++ soc-app-frontend/src/app/home/user-list/user-list.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent implements OnInit {
-
-  constructor() {  }
-
-  ngOnInit() {
-  }};
-
-
+export class UserListComponent {
+  username = 'tom';
+  age: 25;
+}
