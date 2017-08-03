24f1e982a51d9bee9ce0a615b4b9b313f77785d9
fullycrazyma <fullycrazyma@gmail.com>
2017-08-03 03:11:43 +0300

    User service call example

--- soc-app-frontend/src/app/home/user-list/user-list.component.ts
+++ soc-app-frontend/src/app/home/user-list/user-list.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../shared/service/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
   username = 'tom';
   age: 25;
+
+  users;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(
+      (users) => {
+        this.users = users;
+      }
+    )
+  }
 }
