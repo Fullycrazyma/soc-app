b7037cf3752872d3f497d7b71dae3a3dd04dfdfa
fullycrazyma <fullycrazyma@gmail.com>
2017-08-03 04:05:04 +0300

    BE. Swagger integration.

--- soc-app-backend/src/main/java/com/socapp/SocApplication.java
+++ soc-app-backend/src/main/java/com/socapp/SocApplication.java
@@ -9,6 +9,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class SocApplication {
@@ -36,6 +41,19 @@ public class SocApplication {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+        }
+    }
+
+    @Configuration
+    @EnableSwagger2
+    public static class SwaggerConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.basePackage("com.socapp"))
+                    .paths(PathSelectors.any())
+                    .build();
         }
     }
 }
