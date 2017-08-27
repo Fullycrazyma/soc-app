bf9167d4e25190d8f88ab2647c21ab431206de88
p_alekseyenko <p_alekseyenko@wargaming.net>
2017-08-27 17:33:14 +0300

    User service

--- soc-app-frontend/src/app/shared/service/user.service.ts
+++ soc-app-frontend/src/app/shared/service/user.service.ts
@@ -11,11 +11,37 @@ import { User } from './user';
 @Injectable()
 export class UserService {
 
-  constructor(private http: Http) { }
+    private url = '/users';
 
-  getUsers(): Observable<Array<User>> {
-		return this.http.get(environment.apiUrl + '/users/')
-			.map(data => data.json());
-	}
+    constructor(private http: Http) { }
+
+    getUsers(): Observable<Array<User>> {
+        return this.http.get(environment.apiUrl + this.url)
+            .map(res => res.json());
+    }
+
+    getUser(id: string): Observable<User> {
+        return this.http.get(this.getUserUrl(id))
+            .map(res => res.json());
+    }
+
+    addUser(user: User) {
+        return this.http.post(environment.apiUrl + this.url, JSON.stringify(user))
+            .map(res => res.json());
+    }
+
+    updateUser(user: User) {
+        return this.http.put(this.getUserUrl(user.id), JSON.stringify(user))
+            .map(res => res.json());
+    }
+
+    deleteUser(id: string) {
+        return this.http.delete(this.getUserUrl(id))
+            .map(res => res.json());
+    }
+
+    private getUserUrl(id: string) {
+        return environment.apiUrl + this.url + '/' + id;
+    }
 
 }
